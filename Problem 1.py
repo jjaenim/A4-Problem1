2599c48469b0880084a355cf433d3eb79290ec06
jjaenim <joshuatead@gmail.com>
2023-04-22 12:10:30 +0800

    Prints the output

--- Problem 1.py
+++ Problem 1.py
@@ -2,5 +2,5 @@
 # BSCpE 1-4
 # Assignment 4 - Problem 1
 
-# Write each odd number to a new line
-file.write(str(number) + "\n") 
+# prints the output
+print("Even and odd numbers are separated successfully and written to even.txt and odd.txt.")
