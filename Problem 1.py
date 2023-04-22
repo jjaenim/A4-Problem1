64af065959435c43731cc7cdb2534f251aad5854
jjaenim <joshuatead@gmail.com>
2023-04-22 11:57:43 +0800

    Splitting numbers.txt into a list

--- Problem 1.py
+++ Problem 1.py
@@ -2,5 +2,5 @@
 # BSCpE 1-4
 # Assignment 4 - Problem 1
 
-# Open the input file for reading
-with open("C:/assignments_oop/A4-Problem1/numbers.txt", "r") as input:
+# Read the numbers and split them into a list
+numbers = input.read().split() 
