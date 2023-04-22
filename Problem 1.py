4c570b4bc0276f0d65412705e4faf4ca9416668b
jjaenim <joshuatead@gmail.com>
2023-04-22 11:52:21 +0800

    This is the Pseudocode

---
+++ Problem 1.py
@@ -0,0 +1,17 @@
+# Joshua Adrian O. Daet
+# BSCpE 1-4
+# Assignment 4 - Problem 1
+# Pseudocode
+
+
+# Open the input file for reading
+# Read the numbers and separate them into a list
+# Create a list to store even and odd numbers
+# Iterate through the numbers and categorize them as even or odd
+# Convert the number from string to integer
+# Add even numbers to the even_numbers list
+# Add odd numbers to the odd_numbers list
+# Create even.txt and write even numbers to it
+# Write each even number to a new line
+# Create odd.txt and write odd numbers to it
+# Write each odd number to a new line
