4787dca0a617cfd936afbefda9a4811b5e3d7484
jjaenim <joshuatead@gmail.com>
2023-04-22 12:06:19 +0800

    Creates even.txt and write all even numbers to it

--- Problem 1.py
+++ Problem 1.py
@@ -2,5 +2,6 @@
 # BSCpE 1-4
 # Assignment 4 - Problem 1
 
-# Add odd numbers to the odd_numbers list
-odd_numbers.append(number) 
+# Create even.txt and write even numbers to it
+with open("even.txt", "w") as file:
+    for number in even_numbers:
